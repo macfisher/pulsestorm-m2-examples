47a007e0062e468183466d993c7849d03749ab02
macfisher <macfisher01@gmail.com>
2018-07-24 17:16:13 +0000

    TutorialObjectManager2

---
+++ TutorialObjectManager2/registration.php
@@ -0,0 +1,7 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Pulsestorm_TutorialObjectManager2',
+        __DIR__
+    );
+
