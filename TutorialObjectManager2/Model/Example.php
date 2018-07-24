f76f458534357cd2d32cb92d191cb03696cf4f91
macfisher <macfisher01@gmail.com>
2018-07-24 17:38:43 +0000

    M2 DI example

--- TutorialObjectManager2/Model/Example.php
+++ TutorialObjectManager2/Model/Example.php
@@ -4,11 +4,21 @@ namespace Pulsestorm\TutorialObjectManager2\Model;
 class Example
 {
     protected $messageObject;
-	public function __construct()
+
+    // using type hints to trigger automatic dependency injection
+    public function __construct(Message $message) {
+    	var_dump(get_class($message));
+    	exit;
+
+    	$this->messageObject = $message;
+    }
+
+    /** Old constructor before using Magento 2 automatic dependency injection*/
+	/*public function __construct()
 	{
 		$object = new Message; //
 		$this->messageObject = $object;
-	}
+	}*/
 	
     public function sendHelloAgainMessage()
     {
